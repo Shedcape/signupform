344ffc20c200b81e8b7161786a351bbb3a8d5149
Shedcape <shedcapeyt@gmail.com>
2022-06-28 20:24:38 +0200

    Added responsiveness and client-side password validation

--- code.js
+++ code.js
@@ -1,8 +1,20 @@
-const passwords = [document.querySelector('.password'), document.querySelector('.confpassword')];
-passwords.forEach(x => x.addEventListener('click', (e) => {
-    console.log(passwords);
-    if (passwords[0].textContent !== passwords[1].textContent){
-        passwords[0].classList.add('wrongpw');
-        document.querySelector('.pwcheck').style.display = "block"
+const password = document.querySelectorAll('.password');
+password.forEach(x => x.addEventListener('keyup', (e) => {
+    console.log(password)
+    console.log(password[0].value)
+    console.log(password[1].value)
+    let check = document.querySelector('.pwcheck');
+    if (password[0].value !== password[1].value || !validate(password[0].value)){
+        password.forEach(x => x.classList.add('wrongpw'));
+        check.style.visibility = "visible"
+    } else {
+        password.forEach(x => x.classList.remove('wrongpw'));
+        check.style.visibility = "hidden"
     }
 }))
+
+function validate(password) {
+    return /((?!.*\W)(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20})/g.test(password);
+  }
+
+  
