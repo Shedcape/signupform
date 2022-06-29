6e58df44905064624a6ddb8d9706d0d8629ef808
Shedcape <shedcapeyt@gmail.com>
2022-06-29 18:38:04 +0200

    Corrected validation, added mediaquery for mobile view and adjusted html to make it smoother to tab between inputs

--- code.js
+++ code.js
@@ -1,8 +1,5 @@
 const password = document.querySelectorAll('.password');
 password.forEach(x => x.addEventListener('keyup', (e) => {
-    console.log(password)
-    console.log(password[0].value)
-    console.log(password[1].value)
     let check = document.querySelector('.pwcheck');
     if (password[0].value !== password[1].value || !validate(password[0].value)){
         password.forEach(x => x.classList.add('wrongpw'));
@@ -12,9 +9,25 @@ password.forEach(x => x.addEventListener('keyup', (e) => {
         check.style.visibility = "hidden"
     }
 }))
+/* const stars = document.querySelectorAll('input');
+stars.forEach(x => x.addEventListener('keyup', (e) => {
+    let target = e.target.id;
+    let star = document.querySelector(`.${target} > span`);
+    if (e.target.value){
+        star.style.visibility = 'hidden';
+    } else {
+        star.style.visibility = 'visible';
+    }
+})) */
 
 function validate(password) {
     return /((?!.*\W)(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20})/g.test(password);
   }
-
-  
+function formValidation(){
+    stars.forEach(x => {
+        if (x.value === ''){
+            alert(`You need to fill in all fields before submitting`);
+            return false;
+        }
+    })
+};
